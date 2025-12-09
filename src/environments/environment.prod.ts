08b2dae593512095360eaf8d3bca21623aed63c4
Enrique Prada <enrique.pdg@gmail.com>
2025-12-09 12:40:32 -0500

    feat:secreto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   urlEndPoint: "https://abril-delicias-helados-api-beeqh3e8emc5gebr.centralus-01.azurewebsites.net/api",
   chatbotUrl: "https://TU_N8N_EN_NUBE/webhook/chatbot-marcaciones", // Actualizar con URL real de n8n en producción
   azureSpeech: {
-    apiKey: "F1NPdDK4cTvJUx7XBPrZDEEWM8jL7CXL7fwDYWFqNlkjP4Na5NG2JQQJ99BLACYeBjFXJ3w3AAAYACOGyhYm",
+    apiKey: "",
     region: "eastus"
   },
   systemName: "Sistema de Spa",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   urlEndPoint: "http://localhost/proyectoWeb/SistemaMarcacionBackend/api",
   chatbotUrl: "http://localhost:5678/webhook/chatbot-marcaciones",
   azureSpeech: {
-    apiKey: "F1NPdDK4cTvJUx7XBPrZDEEWM8jL7CXL7fwDYWFqNlkjP4Na5NG2JQQJ99BLACYeBjFXJ3w3AAAYACOGyhYm",
+    apiKey: "",
     region: "eastus"
   },
   systemName: "Sistema de Marcaciones",
