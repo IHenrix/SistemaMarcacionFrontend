94785a2f799bfc36a262192b6765953ca201abec
Enrique Prada <enrique.pdg@gmail.com>
2025-12-09 00:47:33 -0500

    feat: modificacion env azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  urlEndPoint: "https://api.spaempresa.com/api-marca-web/",
+  urlEndPoint: "https://abril-delicias-helados-api-beeqh3e8emc5gebr.centralus-01.azurewebsites.net/api",
   systemName: "Sistema de Spa",
   production: true,
   version: "v1.0.0"
